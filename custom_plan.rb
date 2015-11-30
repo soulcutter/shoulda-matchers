674488621033452abf452506213828d22b601187
Elliot Winkler <elliot.winkler@gmail.com>
2015-11-29 19:28:11 -0700

    Goes with Zeus

--- custom_plan.rb
+++ custom_plan.rb
@@ -1,4 +1,5 @@
 require 'zeus/rails'
+require_relative 'spec/support/tests/current_bundle'
 
 class CustomPlan < Zeus::Plan
   def initialize
@@ -7,8 +8,8 @@ class CustomPlan < Zeus::Plan
   end
 
   def boot
-    ENV["BUNDLE_GEMFILE"] = File.expand_path(
-      "../gemfiles/4.2.gemfile",
+    ENV['BUNDLE_GEMFILE'] = File.expand_path(
+      "../gemfiles/#{latest_appraisal}.gemfile",
       __FILE__
     )
 
@@ -24,39 +25,62 @@ class CustomPlan < Zeus::Plan
     # @rails_plan.reconnect_activerecord
   end
 
-  def unit_test_environment
+  def test_environment
     require_relative 'spec/unit_spec_helper'
   end
 
-  def unit_test
-    ARGV.replace(paths_to_run)
+  def rspec
+    ARGV.replace(file_paths_to_run)
     RSpec::Core::Runner.invoke
   end
 
   private
 
-  def paths_to_run
-    if given_paths.empty?
+  def latest_appraisal
+    current_bundle.latest_appraisal
+  end
+
+  def current_bundle
+    Tests::CurrentBundle.instance
+  end
+
+  def file_paths_to_run
+    if given_file_paths.empty?
       ['spec/unit']
     else
-      given_paths.map { |given_path| determine_path_to_run(given_path) }
+      given_file_paths.map do |given_path|
+        determine_file_path_to_run(given_path)
+      end
     end
   end
 
-  def determine_path_to_run(given_path)
-    candidate_path = File.expand_path(
-      "../spec/unit/shoulda/matchers/#{given_path}",
+  def determine_file_path_to_run(given_rspec_argument)
+    expanded_file_path, location =
+      expand_rspec_argument(given_rspec_argument)
+
+    if File.exist?(expanded_file_path)
+      if location
+        expanded_file_path + location
+      else
+        expanded_file_path
+      end
+    else
+      given_rspec_argument
+    end
+  end
+
+  def expand_rspec_argument(rspec_argument)
+    match = rspec_argument.match(/\A(.+?)(:\d+|\[[\d:]+\])?\Z/)
+    file_path, location = match.captures
+    expanded_file_path = File.expand_path(
+      "../spec/unit/shoulda/matchers/#{file_path}",
       __FILE__
     )
 
-    if File.exists?(candidate_path)
-      candidate_path
-    else
-      given_path
-    end
+    [expanded_file_path, location]
   end
 
-  def given_paths
+  def given_file_paths
     ARGV
   end
 end
